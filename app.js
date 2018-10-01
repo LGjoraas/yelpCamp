f14fac3d6d91d66b61959b9a0903804f3e15f1a6
Lindsey Gjoraas <lindseygjoraas@gmail.com>
2018-10-01 20:43:06 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,69 @@
+// npm init
+// npm install express ejs --save
+//setup the root route app.get "/" using res.send
+//make views directory and then create an ejs file for the landing page in ejs
+//create header.ejs and footer.ejs files in views/partials and then put the html header and footer inside
+// add a post route for campgrounds
+//install bodyparser: npm install body-parser --save
+
+//require packages
+var express         = require("express"),
+    app             = express(),
+    bodyParser      = require("body-parser"),
+    mongoose        = require("mongoose"),
+    flash           = require("connect-flash"),
+    passport        = require("passport"),
+    LocalStrategy   = require("passport-local"),
+    methodOverride  = require("method-override"),
+    Campground      = require("./models/campground"),
+    Comment         = require("./models/comment"),
+    User            = require("./models/user"),
+    seedDB          = require("./seeds");
+
+//require routes
+var commentRoutes       = require("./routes/comments"),
+    campgroundRoutes    = require("./routes/campgrounds"),
+    authRoutes          = require("./routes/index");
+
+//use packages
+mongoose.connect('mongodb://localhost/yelp_camp', { useNewUrlParser: true });
+app.use(bodyParser.urlencoded({extended:true}));
+app.set("view engine", "ejs"); // allows not specifying ejs on all the files
+app.use(express.static(__dirname + "/public")); //conventional way of serving the public directory
+app.use(methodOverride("_method"));
+app.use(flash());
+// seedDB(); //seed the database
+
+
+// Passport Configuration
+app.use(require("express-session")({
+    secret: "Once again Rusty wins cutest dog!",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+//check for current user
+app.use(function(req, res, next){
+    res.locals.currentUser = req.user;
+    res.locals.error = req.flash("error");
+    res.locals.success = req.flash("success");
+    next();
+});
+
+//use routes from other files
+app.use(authRoutes);
+app.use("/campgrounds", campgroundRoutes);
+app.use("/campgrounds/:id/comments", commentRoutes);
+
+
+
+//listen for server
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("The yelpcamp server has started!");
+});
---
+++ routes/campgrounds.js
@@ -0,0 +1,102 @@
+var express = require("express"),
+    router = express.Router(),
+    Campground = require("../models/campground"),
+    middleware = require("../middleware"); //will automatically run index.js by default
+
+
+//CAMPGROUND ROUTES
+
+//INDEX - shows all campgrounds
+router.get("/", function(req, res){
+    //get data from db
+    Campground.find({}, function(err, allCampgrounds){
+        if(err){
+            console.log(err);
+        } else {
+            //first parameter in {} is referring to the db name
+            res.render("campgrounds/index", {campgrounds: allCampgrounds}); 
+        }
+    });
+});
+
+//CREATE - posts a new campground to the DB
+router.post("/", middleware.isLoggedIn, function(req, res){
+    // get data from form and add to campgrounds array
+    var name = req.body.name;
+    var price = req.body.price;
+    var image = req.body.image;
+    var desc = req.body.description;
+    var author = {
+        id: req.user._id,
+        username: req.user.username
+    }
+    var newCampground = {name: name, price: price, image: image, description: desc, author: author}
+    //create a new campground and save to db
+    Campground.create(newCampground, function(err, newlyCreated){
+        if(err || !newlyCreated){
+            console.log(err);
+        } else {
+            //redirect back to campgrounds page
+            req.flash("success", "Campground created.");
+            console.log(newlyCreated);
+            res.redirect("/campgrounds");
+        }
+    });
+});
+
+//NEW - shows form to create new campground
+router.get("/new", middleware.isLoggedIn, function(req, res){
+   res.render("campgrounds/new"); 
+});
+
+//SHOW - This shows more info about one campground
+router.get("/:id", function(req, res){
+    //find the campground with provided ID
+    Campground.findById(req.params.id).populate("comments").exec(function(err, foundCampground){
+        if(err || !foundCampground){
+            req.flash("error", "Campground not found.");
+            res.redirect("back");
+        } else {
+            console.log(foundCampground);
+            //render show template
+            res.render("campgrounds/show", {campground: foundCampground});
+        }
+    });
+});
+
+// EDIT - edits the campground
+router.get("/:id/edit", middleware.checkCampgroundOwnership, function(req, res){
+    Campground.findById(req.params.id, function(err, foundCampground){
+        res.render("campgrounds/edit", {campground: foundCampground});
+    });
+});
+
+// UPDATE - updates the campground
+router.put("/:id", middleware.checkCampgroundOwnership, function(req, res){
+   //find and update the correct campground
+   Campground.findByIdAndUpdate(req.params.id, req.body.campground, function(err, updatedCampground){
+       if(err || !updatedCampground){
+           req.flash("error", "Something went wrong. Try again.");
+           res.redirect("/campgrounds");
+       } else {
+           //redirect (show page)
+           req.flash("success", "Campground updated.");
+           res.redirect("/campgrounds/" + req.params.id);
+       }
+   });
+});
+
+// DESTROY - deletes campground route
+router.delete("/:id", middleware.checkCampgroundOwnership, function(req, res){
+    Campground.findByIdAndRemove(req.params.id, function(err){
+        if(err){
+            req.flash("error", "Something went wrong. Try again.");
+            res.redirect("/campgrounds");
+        } else {
+            req.flash("success", "Campground deleted.");
+            res.redirect("/campgrounds");   
+        }
+    });
+});
+
+module.exports = router;
